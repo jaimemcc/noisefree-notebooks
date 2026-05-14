4cb39dcb28c314e4eb27536930e5fd8d4bd1915f
Jaime McCutcheon <j.mccutcheon@uit.no>
2026-05-14 08:37:38 +0200

    new notebook

---
+++ notebooks/text/test_notebook.py
@@ -0,0 +1,29 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.19.2
+#   kernelspec:
+#     display_name: default
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # This is a testing notebook
+# Should see if the syncing works
+
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+
+# %%
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 5, 7, 11]
+
+plt.plot(x, y, marker='o')
+
+# %%
